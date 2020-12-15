ed49d2c5b1dbfcc8c08ba5279f1b9360c6a5897f
dpradell360 <56436797+dpradell360@users.noreply.github.com>
2020-12-15 11:52:21 +0100

    Initial commit

---
+++ Runtime/ExampleClass.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleClass : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("This is an example.");
+    }
+}
